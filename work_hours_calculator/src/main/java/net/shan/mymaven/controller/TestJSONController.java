2b3e5fb00d69ef29e7a769f36f1e1ae5dff5c7df
qd.jiangshan <qd.jiangshan@gmail.com>
2013-12-17 23:30:27 +0800

    Rejected commit: Default

--- work_hours_calculator/src/main/java/net/shan/mymaven/controller/TestJSONController.java
+++ work_hours_calculator/src/main/java/net/shan/mymaven/controller/TestJSONController.java
@@ -1,5 +1,8 @@
 package net.shan.mymaven.controller;
 
+import net.shan.mymaven.domain.AccountVO;
+import net.shan.mymaven.manager.Managers;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,8 @@ import java.util.List;
 @Controller
 @RequestMapping("/")
 public class TestJSONController {
+    private final Logger log = Logger.getLogger(TestJSONController.class);
+
     @RequestMapping(value="/JSON",method=RequestMethod.GET)
     public @ResponseBody Object getJson(){
         System.out.println("===========TestJSONController ==> /JSON ===============");
@@ -41,6 +46,12 @@ public class TestJSONController {
     @RequestMapping(value="/INDEX3",method=RequestMethod.GET)
     public String getIndex3(){
         System.out.println("===========TestJSONController ==> /INDEX3 ===============");
+        AccountVO tmp = Managers.account.check("test1", "test2");
+        if(tmp == null){
+            log.info("login fail.");
+        }else{
+            log.info("login success");
+        }
         return "index3";  //跳转
     }
 }
--- work_hours_calculator/src/main/java/net/shan/mymaven/service/InitContext.java
+++
@@ -1,7 +0,0 @@
-package net.shan.mymaven.service;
-
-public class InitContext {
-    public void init(){
-        System.out.println("=========InitContext --> init===============");
-    }
-}
